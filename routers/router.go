538bac23ca84d16793a54baab1c1c36a6c256ecc
kebikeyi <285273820@qq.com>
2015-10-12 15:49:18 +0800

    routers/router.go

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,25 @@ package routers
 
 import (
 	"GoodsManage/controllers"
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
+var FilterUser = func(ctx *context.Context) {
+	uid, ok := ctx.Input.Session("uid").(int64)
+	fmt.Println("auth check:", uid, ok)
+	if !ok {
+		// ctx.WriteString("<script language='javascript'>window.top.location='/login.html';</script>")
+		ctx.Redirect(302, "/login")
+		return
+	}
+}
+
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.InsertFilter("/main", beego.BeforeRouter, FilterUser)
+	//beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.LoginController{})
+	beego.Router("/login", &controllers.LoginController{})
+	beego.AutoRouter(&controllers.LoginController{})
 }
