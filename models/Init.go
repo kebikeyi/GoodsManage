2ef3d92565750162cf1c0d0b55737fa92121be6c
kebikeyi <285273820@qq.com>
2015-11-02 17:16:09 +0800

    models/Init.go models/Tuser.go

--- models/Init.go
+++ models/Init.go
@@ -14,7 +14,7 @@ var Usera *Tuser
 
 func init() {
 	var err error
-	X, err = xorm.NewEngine("mssql", "server=.;database=tests;user id=sa;password=111111")
+	X, err = xorm.NewEngine("mssql", "server=.;database=goodsmanage;user id=sa;password=111111")
 	fmt.Println("-------11111")
 	if err != nil {
 		log.Fatalf("failt to carete engine")
@@ -25,10 +25,23 @@ func init() {
 	if err = X.Sync2(new(Tuser)); err != nil {
 		log.Fatalf("failt Tuser to sync", err.Error())
 	}
-	//初始管理员
-	var tuser = new(Tuser).Init()
-	var d = tuser.GetUser("admin")
-	if d == false {
-		_, _ = X.Exec("insert into [Tuser](uname,password)values('admin','admin')")
+	//菜单表
+	if err = X.Sync2(new(TMenus)); err != nil {
+		log.Fatalf("failt TMenus to sync", err.Error())
 	}
+	//角色表
+	if err = X.Sync2(new(TRoles)); err != nil {
+		log.Fatalf("failt TRoles to sync", err.Error())
+	}
+	//初始管理员
+	// var tuser = new(Tuser).Init()
+	// var d = tuser.GetUser("admin")
+	// if d == false {
+	// 	_, _ = X.Exec("insert into [Tuser]([uname],password)values('admin','admin')")
+	// }
+	var tuser = new(Tuser).Init()
+	if !tuser.Get(1) {
+		_, _ = X.Exec("insert into [Tuser]([uname],password)values('admin','admin')")
+	}
+
 }
--- models/Tuser.go
+++ models/Tuser.go
@@ -5,10 +5,18 @@ import (
 )
 
 type Tuser struct {
-	ID       int64
-	UNAME    string `xorm:"varchar(50)"` //用户名
-	PASSWORD string `xorm:"varchar(50)"` //密码
-	_godb    `xorm:"-"`
+	ID        int64
+	UNAME     string `xorm:"varchar(50)"` //用户名
+	PASSWORD  string `xorm:"varchar(50)"` //密码
+	Type      int    //
+	AddTime   string `xorm:"varchar(8)"`    //添加时间
+	Rights    string `xorm:"varchar(2000)"` //权限
+	Roles     string `xorm:"varchar(500)"`  //角色
+	LoginTime string `xorm:"varchar(8)"`    //登录时间
+	LoginIP   string `xorm:"varchar(50)"`   //登录IP
+	Memo      string `xorm:"varchar(200)"`  //
+	State     int    //
+	_godb     `xorm:"-"`
 }
 
 func (this *Tuser) Init() *Tuser {
@@ -19,7 +27,7 @@ func (this *Tuser) Init() *Tuser {
 
 //登录
 func (this *Tuser) Login(uname string, psw string) bool {
-	has, err := X.Where("name=? and psw=?", uname, psw).Get(this)
+	has, err := X.Where("uname=? and password=?", uname, psw).Get(this)
 	fmt.Println(has)
 	if err != nil {
 		fmt.Println("------11111")
@@ -30,7 +38,7 @@ func (this *Tuser) Login(uname string, psw string) bool {
 
 //获取用户
 func (this *Tuser) GetUser(uname string) bool {
-	has, err := X.Where("name=?", uname).Get(this)
+	has, err := X.Where("uname=?", uname).Get(this)
 	if err != nil {
 		return false
 	}
