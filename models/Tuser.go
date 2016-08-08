b922cca1a2b9187a4d14513afc1a64f740606993
kebikeyi <285273820@qq.com>
2016-08-08 23:30:31 +0800

    models/Tuser.go

--- models/Tuser.go
+++ models/Tuser.go
@@ -8,6 +8,7 @@ type Tuser struct {
 	ID        int64
 	UNAME     string `xorm:"varchar(50)"` //用户名
 	PASSWORD  string `xorm:"varchar(50)"` //密码
+	Nick      string `xorm:"varchar(32)"`
 	Type      int    //
 	AddTime   string `xorm:"varchar(8)"`    //添加时间
 	Rights    string `xorm:"varchar(2000)"` //权限
@@ -39,6 +40,16 @@ func (this *Tuser) Login(uname string, psw string) bool {
 //获取用户
 func (this *Tuser) GetUser(uname string) bool {
 	has, err := X.Where("uname=?", uname).Get(this)
+	if err != nil {
+		return false
+	}
+	return has
+}
+
+//获取原始密码
+func (this *Tuser) HuoQU(pwd string) bool {
+	has, err := X.Where("password=?", pwd).Get(this)
+	fmt.Println("has is", has)
 	if err != nil {
 		return false
 	}
