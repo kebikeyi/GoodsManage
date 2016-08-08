9f4c4172452112eb6e126c216247dd759c6de8a5
kebikeyi <285273820@qq.com>
2016-08-08 23:27:50 +0800

    controllers/user.go

---
+++ controllers/user.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	//"GoodsManage/controllers"
+	//"fmt"
+	//"GoodsManage/models"
+
+	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego/context"
+)
+
+type UserController struct {
+	beego.Controller
+}
+
+func (this *UserController) get() {
+
+}
