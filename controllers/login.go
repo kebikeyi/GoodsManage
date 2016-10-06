24e3a16e9fe74f87d5dd7c3d4808df076818e1ca
kebikeyi <285273820@qq.com>
2016-10-06 23:07:15 +0800

    login.go

--- controllers/login.go
+++ controllers/login.go
@@ -11,69 +11,77 @@ import (
 	_ "github.com/astaxie/beego/session"
 )
 
+//LoginController 控制器
 type LoginController struct {
 	beego.Controller
 }
 
-func (this *LoginController) Get() {
-	this.Data["Website"] = "beego.me"
-	this.Data["Email"] = "astaxie@gmail.com"
+// Get 登录
+func (my *LoginController) Get() {
+	my.Data["Website"] = "beego.me"
+	my.Data["Email"] = "astaxie@gmail.com"
 	var user = new(Tuser).Init()
 	fmt.Println("login.user", user.Query("select * from Tuser"))
 	fmt.Println("login.Service.User", user.Query("select * from Tuser"))
 	fmt.Println("login.Service.User.Get(1)", user.Get(1))
-	this.TplNames = "login/login.html"
+	my.TplName = "login/login.html"
 }
-func (this *LoginController) Post() {
-	var username = this.GetString("uname")
-	var password = this.GetString("psw")
+
+//Post 提交
+func (my *LoginController) Post() {
+	var username = my.GetString("uname")
+	var password = my.GetString("psw")
 
 	var user = new(Tuser).Init()
 	var b = user.Login(username, password)
 	fmt.Println(user)
 	fmt.Println(b)
 	if false == b {
-		this.Ctx.WriteString("alert('用户名或密码有误！');")
+		my.Ctx.WriteString("alert('用户名或密码有误！');")
 		return
 	}
-	this.SetSession("uid", user.ID)
-	this.SetSession("uname", username)
-	// this.Ctx.SetCookie("uid", user.ID)
-	// this.Ctx.SetCookie("uname", username)
-	this.Ctx.WriteString("1")
+	my.SetSession("uid", user.ID)
+	my.SetSession("uname", username)
+	// my.Ctx.SetCookie("uid", user.ID)
+	// my.Ctx.SetCookie("uname", username)
+	my.Ctx.WriteString("1")
 
 }
-func (this *LoginController) Uppwd() {
+
+// 密码修改
+func (my *LoginController) Uppwd() {
 	//this.Layout = "main/index.html"
-	this.TplNames = "login/uppwd.html"
+	my.TplName = "login/uppwd.html"
 }
-func (this *LoginController) UppwdPost() {
-	var pwd = this.GetString("pwd")
-	var pwd1 = this.GetString("pwd1")
-	var pwd2 = this.GetString("pwd2")
-	//var id = this.GetSession("uid")
-	//var id = this.Ctx.GetCookie("uid")
-	var id, _ = this.GetInt64("id", 0)
+
+// UppwdPost 修改提交
+func (my *LoginController) UppwdPost() {
+	var pwd = my.GetString("pwd")
+	var pwd1 = my.GetString("pwd1")
+	var pwd2 = my.GetString("pwd2")
+	//var id = my.GetSession("uid")
+	//var id = my.Ctx.GetCookie("uid")
+	var id, _ = my.GetInt64("id", 0)
 	fmt.Println("id is:", id)
 	// var uid, _ = strconv.ParseInt(id, 10, 64)
 	// fmt.Println("uid is", uid)
 	fmt.Println(pwd, pwd1, pwd2)
 	if pwd1 == "" || pwd2 == "" || pwd1 != pwd2 {
-		this.Ctx.WriteString("alert('两次密码不形同！');")
+		my.Ctx.WriteString("alert('两次密码不形同！');")
 	}
 	var user = new(Tuser).Init()
 
 	var b = user.HuoQU(pwd)
 	if b == false {
-		this.Ctx.WriteString("alert('原密码不正确！');")
+		my.Ctx.WriteString("alert('原密码不正确！');")
 	}
 	user.PASSWORD = pwd1
 	var h = user.Update(id)
 	fmt.Println()
 	if h > 0 {
-		this.Ctx.WriteString("alert('修改成功！');")
+		my.Ctx.WriteString("alert('修改成功！');")
 	} else {
-		this.Ctx.WriteString("alert('修改失败！');")
+		my.Ctx.WriteString("alert('修改失败！');")
 	}
 
 }
